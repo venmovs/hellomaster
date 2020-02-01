2e7dbd494009355b84609db8a8e6760f626b65b0
vigen94 <vigen94@icloud.com>
2020-02-01 16:08:42 +0300

    init git

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+/*
+let btn = document.getElementById('service__modal'),
+    modal = document.querySelector('.modal'),
+    btnOf = document.querySelector('.button_of');
+
+btn.addEventListener('click', function (event) {
+    if (modal.style.display == 'none' && btnOf.style.display == 'none'){
+        modal.style.display = 'block';
+        btnOf.style.display = 'block';
+    }else {
+        modal.style.display = 'none';
+        btnOf.style.display = 'none';
+    }
+});
+*/
+
+
+
+
+/*$('[data-modal=service__modal]').on('click', function () {
+    $('.modal, .button_of').fadeTo(100, 1);
+});
+$('.modal__close').on('click', function () {
+    $('.modal, .button_of').fadeTo(100, 0);
+});*/

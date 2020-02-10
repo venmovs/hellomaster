3e1b9d4c72e5aa73c4fb858cdb0251b30f24fc20
vigen <vigen94@icloud.com>
2020-02-10 09:43:58 +0300

    js_add_end

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,55 @@
-/*
-let btn = document.getElementById('service__modal'),
-    modal = document.querySelector('.modal'),
-    btnOf = document.querySelector('.button_of');
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
 
-btn.addEventListener('click', function (event) {
-    if (modal.style.display == 'none' && btnOf.style.display == 'none'){
-        modal.style.display = 'block';
-        btnOf.style.display = 'block';
-    }else {
-        modal.style.display = 'none';
-        btnOf.style.display = 'none';
-    }
-});
+/*
+    тоглил бохы с услугами
 */
 
+    let serviceBox = document.querySelectorAll(".service__box");
+    for (let box of serviceBox) {
+        box.addEventListener('click', function () {
+            box.classList.toggle("service__box_active");
+        });
+    }
+
+/*
+    появление инпутов при нажатии на время
+*/
+
+    let signBox = document.querySelectorAll(".sign__box"),
+        signForm = document.querySelector('.sign__wrapper__hidden'),
+        scrolling = 0;
 
 
+    for (let box of signBox){
+        box.addEventListener('click', function () {
+            signBox.forEach(n => n.classList.remove('sign__box__active'));
+            box.classList.toggle("sign__box__active");
+            let boxPos = box.getBoundingClientRect().y;
+            let formPos = signForm.getBoundingClientRect().y;
+            let dif = formPos - boxPos;
+            if (dif < 20){
+                    window.scrollBy(0, Math.abs(dif) + 60)
+            }
+            signForm.classList.add("sign__wrapper__active");
+            scrolling = pageYOffset;
 
-/*$('[data-modal=service__modal]').on('click', function () {
-    $('.modal, .button_of').fadeTo(100, 1);
+        });
+    }
+
+/*
+    исчизновение при скролле
+*/
+    window.addEventListener('scroll', function() {
+        let difference = scrolling - pageYOffset;
+        if (difference < -50 || difference > 50){
+            signForm.classList.remove('sign__wrapper__active');
+        }
+    });
 });
-$('.modal__close').on('click', function () {
-    $('.modal, .button_of').fadeTo(100, 0);
-});*/
+
+
+
+
+
+
